34a19251624317b921b5e86fecf4bd779f7dc489
UsamaMunawarRajpoot <rajpootusama329@gmail.com>
2023-03-30 17:47:46 +0500

    1st change

---
+++ R_Streamlit.py
@@ -0,0 +1,20 @@
+import streamlit as st
+from PIL import Image
+
+#title
+st.title('R IN RAMADAN')
+
+
+image1 =Image.open("coding.png")
+st.image(image1)
+
+
+
+# Add Video
+st.write('''
+## Day 1 (a)  
+Installation Procedure''')
+video1 = open("Installation.mp4","rb")
+st.video(video1)
+
+
